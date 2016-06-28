cd06ff842d6255a9646f07f1187e3fabb8bcdb1d
Nikita Ermolenko <nikita.ermolenko@rosberry.com>
2016-06-28 09:04:19 +0600

    Add tests for top/bottom and right/left relations

---
+++ Example/Tests/NUIFramerRelationLogicSpec.m
@@ -0,0 +1,53 @@
+//
+//  NUIFramerRelationLogicSpec.m
+//  Framer
+//
+//  Created by Nikita Ermolenko on 28/06/16.
+//  Copyright © 2016 Nikita Ermolenko. All rights reserved.
+//
+
+#import <Framer/Framer.h>
+#import <Kiwi/Kiwi.h>
+
+SPEC_BEGIN(RELATIONSPEC)
+
+describe(@"Framer", ^{
+   
+    UIView *mainView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 500, 500)];
+    UIView *testingView = [[UIView alloc] init];
+    
+    beforeAll(^{
+        [mainView addSubview:testingView];
+    });
+    
+    beforeEach(^{
+        testingView.frame = CGRectZero;
+    });
+    
+    context(@"should correctly builds relations", ^{
+        
+        it(@"for 'bottom' and 'top' configurations together", ^{
+            [testingView installFrames:^(NUIFramer * _Nonnull framer) {
+                framer.super_centerX(0);
+                framer.width(100);
+                framer.bottom(10).and.top(10);
+            }];
+            
+            [[theValue(testingView.frame) should] equal:theValue(CGRectMake(200, 10, 100, 480))];
+        });
+        
+        it(@"for 'right' and 'left' configurations together", ^{
+            [testingView installFrames:^(NUIFramer * _Nonnull framer) {
+                framer.super_centerY(0);
+                framer.height(100);
+                framer.left(10).and.right(10);
+            }];
+            
+            [[theValue(testingView.frame) should] equal:theValue(CGRectMake(10, 200, 480, 100))];
+        });
+        
+    });
+    
+});
+
+SPEC_END
