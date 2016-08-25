57728f02889fd41d4fe9fe9246f5f71ee4a6440a
Nikita Ermolenko <nikita.ermolenko@rosberry.com>
2016-08-25 14:15:30 +0600

    Update local pod

--- Example/Pods/Target Support Files/Framer/Framer-umbrella.h
+++ Example/Pods/Target Support Files/Framer/Framer-umbrella.h
@@ -1,8 +1,10 @@
 #import <UIKit/UIKit.h>
 
 #import "Framer.h"
+#import "Macroses.h"
 #import "NUIFramer.h"
 #import "NUIHandler.h"
+#import "NUIHandlerInfo.h"
 #import "UIView+NUIAdditions.h"
 #import "UIView+NUIInstaller.h"
 
